f6f26b739c1c61342d26b58065c6a36a6ab38a39
remi <remi.tondeux@gmail.com>
2021-12-12 19:13:32 +0100

    getting 25 nice cards

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,21 @@
+require 'csv'
+
 class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    get25names
+  end
+
+  private
+
+  def get25names
+    filepath = '././db/nomsCommuns.csv'
+    @all_names = []
+    CSV.foreach(filepath) do |row|
+      @all_names << row[0]
+    end
+    @all_names.shuffle!
+    @names = @all_names.take(25)
   end
 end
