89db0ed383b8f48dbbc0afa24e4b1a6e66a03f7e
remi <remi.tondeux@gmail.com>
2021-12-12 23:13:21 +0100

    implement card color logic

--- app/javascript/controllers/return_card_controller.js
+++ app/javascript/controllers/return_card_controller.js
@@ -4,8 +4,18 @@ export default class extends Controller {
   static targets = ['cards'];
 
   return(event) {
-    console.log(event)
-    this.cardsTarget.classList.add("red-card");
-    this.cardsTarget.innerText = ""
+    this.cardsTarget.classList.remove("cards");
+    if (this.cardsTarget.id == 1) {
+      this.cardsTarget.classList.add("black-card");
+    } else if (this.cardsTarget.id < 9) {
+      this.cardsTarget.classList.add("red-card");
+      this.cardsTarget.innerText = "";
+    } else if (this.cardsTarget.id < 17) {
+      this.cardsTarget.classList.add("blue-card");
+      this.cardsTarget.innerText = "";
+    } else {
+      this.cardsTarget.classList.add("grey-card");
+      this.cardsTarget.innerText = "";
+    };
   }
 }
