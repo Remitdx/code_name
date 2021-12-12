7eed10d1f63fd8dcf9b3e42475650592de6f929e
remi <remi.tondeux@gmail.com>
2021-12-12 17:59:35 +0100

    finding some text resources and generate games controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Game.destroy_all # if Rails.env.development?
+User.destroy_all # if Rails.env.development?
+
+puts 'Creating a game'
+Game.create()
+
+puts 'Creating some users'
+user1 = User.create!(email: "florimond@gmail.com", password: "azerty", pseudo: "Flori")
+user2 = User.create!(email: "remi@gmail.com", password: "azerty", pseudo: "Kodoliprane")
+user3 = User.create!(email: "fx@wagon.com", password: "azerty", pseudo: "FX")
+
+puts "everything worked!"
